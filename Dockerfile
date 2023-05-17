b4e6f93a2ac2bc1bf33d6d179d3328bc33a1d319
ion <ion.boleac2@gmail.com>
2023-05-17 16:37:50 +0200

    Commit finale

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM ubuntu
 
 WORKDIR /home/Champagne_Shop
 
--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+/home/Champagne_Shop/glassfish5/bin/asadmin start-domain;
+# /home/Champagne_Shop/glassfish5/bin/asadmin undeploy Champagne_Shop-1.0-SNAPSHOT;
+# /home/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin --contextroot Champagne_Shop deploy /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war
+/home/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin deploy --contextroot Champagne_Shop /home/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war;
 
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin stop-domain
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin start-domain
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin undeploy Champagne_Shop-1.0-SNAPSHOT
-# /home/ion/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin --contextroot Champagne_Shop deploy /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin deploy /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war
+/bin/bash
