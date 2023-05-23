ad55ae753de43428ab63f7d9aaaec856b3c15509
ion <ion.boleac2@gmail.com>
2023-05-23 16:05:52 +0200

    wdewfefdsfsd

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-/home/Champagne_Shop/glassfish5/bin/asadmin start-domain;
-# /home/Champagne_Shop/glassfish5/bin/asadmin undeploy Champagne_Shop-1.0-SNAPSHOT;
-# /home/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin --contextroot Champagne_Shop deploy /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war
-/home/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin deploy --contextroot Champagne_Shop /home/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war;
+/home/ion/Champagne_Shop/glassfish5/bin/asadmin start-domain;
+
+/home/ion/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin deploy --contextroot Champagne_Shop /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war;
 
 /bin/bash
