6c697103fcf13bda6acf1f35442051d5d9389acf
ion <ion.boleac2@gmail.com>
2023-06-01 11:11:22 +0200

    mnodifice

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin start-domain;
+/home/ion/Champagne_Shop/glassfish5_new/bin/asadmin start-domain;
 
-/home/ion/Champagne_Shop/glassfish5/bin/asadmin --interactive=true --host localhost --port 4848 --user admin deploy --contextroot Champagne_Shop /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war;
+/home/ion/Champagne_Shop/glassfish5_new/bin/asadmin --interactive=true --host localhost --port 4848 --user admin deploy --contextroot Champagne_Shop /home/ion/Champagne_Shop/target/Champagne_Shop-1.0-SNAPSHOT.war;
 
 /bin/bash
--- src/test/java/model/mySQLJDBCImpl/CarrelloDAOMySQLJDBCImplTest.java
+++ src/test/java/model/mySQLJDBCImpl/CarrelloDAOMySQLJDBCImplTest.java
@@ -1,6 +1,9 @@
 package model.mySQLJDBCImpl;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -27,13 +30,14 @@ public class CarrelloDAOMySQLJDBCImplTest {
     static void init() {
         System.out.println("CarrelloDAOMySQLJDBCImplTest");
 
-        utente = new Utente();
-        prodotto = new Prodotto();
+        
     }
 
     @Test
     @DisplayName("Test addProdottoNelCarrello con utente e prodotto esistenti")
     void testAddProdottoNelCarrello() throws DuplicatedObjectException{
+        utente = new Utente();
+        prodotto = new Prodotto();
         Carrello carrello;
         DAOFactory daoFactory = DAOFactory.getDAOFactory(Configuration.DAO_IMPL, null);
         assert daoFactory != null;
@@ -44,8 +48,17 @@ public class CarrelloDAOMySQLJDBCImplTest {
         utente.setEmail("ion.boleac@gmail.com");
         prodotto.setIdProdotto(Long.valueOf(1));
 
+        int quantita = carrelloDAO.getQuantitaProdottoNelCarrello(utente, prodotto);
+
 
         carrelloDAO.addProdottoNelCarrello(utente, prodotto, 2, 200);
+
+
+        int quantita1 = carrelloDAO.getQuantitaProdottoNelCarrello(utente, prodotto);
+
+        assertEquals(quantita + 2, quantita1);
+
+        
 
         daoFactory.rollbackTransaction();
         daoFactory.closeTransaction();
@@ -54,6 +67,8 @@ public class CarrelloDAOMySQLJDBCImplTest {
     @Test
     @DisplayName("Test addProdottoNelCarrello con utente e prodotto non esistenti")
     void testAddProdottoNelCarrello2() throws DuplicatedObjectException{
+        utente = new Utente();
+        prodotto = new Prodotto();
         Carrello carrello;
         DAOFactory daoFactory = DAOFactory.getDAOFactory(Configuration.DAO_IMPL, null);
         assert daoFactory != null;
@@ -70,7 +85,24 @@ public class CarrelloDAOMySQLJDBCImplTest {
                 "Expected addProdottoNelCarrello() to throw, but it didn't"
         );
 
+        //carrelloDAO.addProdottoNelCarrello(utente, prodotto, 2, 200),
+
         daoFactory.rollbackTransaction();
         daoFactory.closeTransaction();
+    }
+
+    @Test
+    @DisplayName("Test getQuantitaProdottoNelCarrello con utente e prodotto esistenti")
+    void testGetQuantitaProdottoNelCarrello() throws DuplicatedObjectException{
+        utente = new Utente();
+        prodotto = new Prodotto();
+        Carrello carrello;
+        DAOFactory daoFactory = DAOFactory.getDAOFactory(Configuration.DAO_IMPL, null);
+        assert daoFactory != null;
+        daoFactory.beginTransaction();
+
+        CarrelloDAO carrelloDAO = daoFactory.getCarelloDAO();
+
+        utente.setEmail("email@gmail.com");
     }
 }
